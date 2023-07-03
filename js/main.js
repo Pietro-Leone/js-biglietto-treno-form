eca99dacf25c34b0fd60ff9abb05666355b8b182
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-03 17:39:42 +0200

    fine form

--- js/main.js
+++ js/main.js
@@ -1,30 +1,45 @@
 // Dichiarazione Variabili e Costanti
-const kilometers = 20;
-const userAge = 20;
-let discount = 0;
+const nameInputElement = document.getElementById("inputName");
+const kilometersInputElement = document.getElementById("inputKilometers");
+const ageInputElement = document.getElementById("inputAge");
+const biglietto = document.getElementById("biglietto")
+
 const date = new Date();
 const currentDay = date.getDate();
 const currentMonth = date.getUTCMonth() + 1;
 const currentYear = date.getFullYear();
 const currentDate = ` ${currentDay}/${currentMonth}/${currentYear} `
-
-// Validazione dati inseriti
-if (isNaN(kilometers) || kilometers <= 0 || isNaN(userAge)) {
-    alert("Ricarica la pagina e inserisci dei dati validi");
-}
+const btnGenerate = document.querySelector(".genera-button");
 
 document.getElementById("date").innerHTML = `${currentDate}`
 
-// Calcolo kilometri * prezzo al kilometro
-const kmPrice = kilometers * 0.21;
+btnGenerate.addEventListener("click", function () {
+    biglietto.classList.replace("d-none", "d-block");
+    const name = nameInputElement.value;
+    document.getElementById("name").innerHTML = `${name}`
 
-// Blocco IF che in base all'età inserita calcola eventuali sconti
-if (userAge < 18){
-    document.getElementById("age").innerHTML = "Bambino"
-    discount = 0.2
-}
-else if (userAge >= 65){
-    discount = 0.4
-}
+    const kilometers = kilometersInputElement.value;
+    const userAge = ageInputElement.value;
 
-document.getElementById("price").innerHTML = `${(kmPrice - (kmPrice * discount)).toFixed(2)}€`
+    let discount = 0;
+
+    // Validazione dati inseriti
+    if (isNaN(kilometers) || kilometers <= 0 || isNaN(userAge)) {
+        alert("Ricarica la pagina e inserisci dei dati validi");
+    }
+
+    // Calcolo kilometri * prezzo al kilometro
+    const kmPrice = kilometers * 0.21;
+
+    // Blocco IF che in base all'età inserita calcola eventuali sconti
+    if (userAge == 1) {
+        document.getElementById("age").innerHTML = "Bambino"
+        discount = 0.2
+    }
+    else if (userAge == 3) {
+        discount = 0.4
+    }
+
+    document.getElementById("price").innerHTML = `${(kmPrice - (kmPrice * discount)).toFixed(2)}€`
+
+})
