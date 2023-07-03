fec6b1684c47286eecee742feed18148b315c4c2
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-03 18:27:31 +0200

    Fatti i bonus

--- js/main.js
+++ js/main.js
@@ -3,20 +3,31 @@ const nameInputElement = document.getElementById("inputName");
 const kilometersInputElement = document.getElementById("inputKilometers");
 const ageInputElement = document.getElementById("inputAge");
 const biglietto = document.getElementById("biglietto")
-
 const date = new Date();
 const currentDay = date.getDate();
 const currentMonth = date.getUTCMonth() + 1;
 const currentYear = date.getFullYear();
 const currentDate = ` ${currentDay}/${currentMonth}/${currentYear} `
 const btnGenerate = document.querySelector(".genera-button");
+const btnReset = document.querySelector(".reset-button");
 
 document.getElementById("date").innerHTML = `${currentDate}`
 
+// Evento al click del bottone "Reset"
+btnReset.addEventListener("click", function () {
+    biglietto.classList.replace("d-block", "d-none");
+})
+
+// Evento al click del bottone "Genera"
 btnGenerate.addEventListener("click", function () {
     biglietto.classList.replace("d-none", "d-block");
     const name = nameInputElement.value;
     document.getElementById("name").innerHTML = `${name}`
+
+    // Randomizzo il cp
+    const randomNum = Math.floor(Math.random() * 900000) + 100000;
+    document.getElementById("random").innerHTML = `${randomNum}`
+
 
     const kilometers = kilometersInputElement.value;
     const userAge = ageInputElement.value;
